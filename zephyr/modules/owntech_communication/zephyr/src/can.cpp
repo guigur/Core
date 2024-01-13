7ef98dd5132e9150e90c177a8a30d507e94eda1c
Luiz Villa <lflavado@laas.fr>
2024-01-13 01:38:09 +0100

    Modifications to the thingset project to enable can communication

--- zephyr/modules/owntech_communication/zephyr/src/can.cpp
+++ zephyr/modules/owntech_communication/zephyr/src/can.cpp
@@ -1,11 +1,15 @@
 
 
 #include "can.h"
-
+// #include "GpioApi.h"
 #include <zephyr/drivers/gpio.h>
 
 void enable_can()
 {
 	const struct gpio_dt_spec can_standby_spec = GPIO_DT_SPEC_GET(CAN_STANDBY_DEVICE, gpios);
 	gpio_pin_configure_dt(&can_standby_spec, GPIO_OUTPUT_INACTIVE);
+
+    // gpio.configurePin(PB10,OUTPUT);
+    // gpio.resetPin(PB10);
+
 }
