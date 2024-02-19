502f69eef3158d23a28d61ad45b1e66f4e83d14d
Luiz Villa <lflavado@laas.fr>
2024-02-19 17:32:25 +0100

    Updated the main.cpp to have a generic communicaiton protocol with boost mode and low level power control

--- src/main.cpp
+++ src/main.cpp
@@ -32,6 +32,10 @@
 #include "TaskAPI.h"
 #include "TwistAPI.h"
 #include "DataAPI.h"
+
+
+#include "opalib_control_pid.h"
+
 //------------ZEPHYR DRIVERS----------------------
 #include "zephyr/zephyr.h"
 #include "zephyr/console/console.h"
@@ -45,12 +49,23 @@
 
 #define LEG1_CAPA_DGND PC6
 #define LEG2_CAPA_DGND PB7
+#define LEG1_DRIVER_SWITCH PC12
+#define LEG2_DRIVER_SWITCH PB13
 
 #define BOOL_SETTING_OFF 0
 #define BOOL_SETTING_ON 1
 
-#define LEG_1 0
-#define LEG_2 1
+#define BOOL_LEG 0
+#define BOOL_CAPA 1
+#define BOOL_DRIVER 2
+#define BOOL_BUCK 3
+#define BOOL_BOOST 4
+
+#define CAPA_SWITCH_INDEX 0
+#define DRIVER_SWITCH_INDEX 1
+
+// #define LEG1 0
+// #define LEG2 1
 
 
 
@@ -64,8 +79,8 @@ void loop_control_task();     // Code to be executed in real time in the critica
 
 //----------------SERIAL PROTOCOL HANDLER FUNCTIONS---------------------
 void console_read_line();
+
 void defaultHandler();
-void powerHandler();
 void powerLegSettingsHandler();
 
 void boolSettingsHandler(uint8_t power_leg, uint8_t setting_position);
@@ -73,18 +88,25 @@ void dutyHandler(uint8_t power_leg, uint8_t setting_position);
 void referenceHandler(uint8_t power_leg, uint8_t setting_position);
 void calibrationHandler();
 
+
+
 //--------------USER VARIABLES DECLARATIONS----------------------
 
 
 
 static uint32_t control_task_period = 100; //[us] period of the control task
-static bool pwm_enable = false;            //[bool] state of the PWM (ctrl task)
+static bool pwm_enable_leg_1 = false;            //[bool] state of the PWM (ctrl task)
+static bool pwm_enable_leg_2 = false;            //[bool] state of the PWM (ctrl task)
 
 uint8_t received_serial_char;
 uint8_t received_char;
 char bufferstr[255]={};
 bool print_done = false;
 
+/* PID coefficient for a 8.6ms step response*/
+static float32_t kp = 0.000215;
+static float32_t ki = 2.86;
+static float32_t kd = 0.0;
 
 /* Measure variables */
 
@@ -158,21 +180,28 @@ TrackingVariables tracking_vars[] = {
 uint8_t num_tracking_vars = 6;
 
 // Define a struct to hold the settings of each power leg
+// settings ----
+// bool leg_on    - to define if the leg is ON or OFF
+// bool capa_on   - to define if the capacitor of the leg is ON or OFF
+// bool driver_on - to define if the driver of the leg is ON or OFF
+// bool buck_mode - to define is the leg is in buck mode
+// bool boost_mode - to define is the leg is in boost mode
+// switches ----
+// pin_t CAPA_SWITCH    - holds the pin of the capa
+// pint_t DRIVER_SWITCH - holds the pin of the driver
 typedef struct {
-    // bool leg_on    - to define if the leg is ON or OFF
-    // bool capa_on   - to define if the capacitor of the leg is ON or OFF
-    // bool driver_on - to define if the driver of the leg is ON or OFF
-    // bool buck_mode - to define is the leg is in buck mode or boost mode
-    bool settings[4];
+    bool settings[5];
+    pin_t switches[2];
     float32_t *tracking_variable;
+    const char *tracking_var_name;
     float32_t reference_value;
     float32_t duty_cycle;
 } PowerLegSettings;
 
 PowerLegSettings power_leg_settings[] = {
-    //   LEG_OFF      ,   CAPA_OFF      , DRIVER_OFF      ,  BUCK_MODE_ON
-    {{BOOL_SETTING_OFF, BOOL_SETTING_OFF, BOOL_SETTING_OFF,  BOOL_SETTING_ON},  &V1_low_value, reference_value, starting_duty_cycle},
-    {{BOOL_SETTING_OFF, BOOL_SETTING_OFF, BOOL_SETTING_OFF,  BOOL_SETTING_ON},  &V2_low_value, reference_value, starting_duty_cycle}
+    //   LEG_OFF      ,   CAPA_OFF      , DRIVER_OFF      ,  BUCK_MODE_ON,  BOOST_MODE_ON
+    {{BOOL_SETTING_OFF, BOOL_SETTING_OFF, BOOL_SETTING_OFF,  BOOL_SETTING_OFF,  BOOL_SETTING_OFF}, {LEG1_CAPA_DGND, LEG1_DRIVER_SWITCH},  &V1_low_value, "V1", reference_value, starting_duty_cycle},
+    {{BOOL_SETTING_OFF, BOOL_SETTING_OFF, BOOL_SETTING_OFF,  BOOL_SETTING_OFF,  BOOL_SETTING_OFF}, {LEG2_CAPA_DGND, LEG2_DRIVER_SWITCH},  &V2_low_value, "V2",reference_value, starting_duty_cycle}
 };
 
 typedef struct {
@@ -185,6 +214,7 @@ cmdToSettings_t power_settings[] = {
     {"_c", boolSettingsHandler},
     {"_v", boolSettingsHandler},
     {"_b", boolSettingsHandler},
+    {"_t", boolSettingsHandler},
     {"_r", referenceHandler},
     {"_d", dutyHandler},
 };
@@ -195,13 +225,12 @@ uint8_t num_power_settings =  sizeof(power_settings)/sizeof(power_settings[0]);
 typedef struct {
     char cmd[16];
     tester_states_t mode;
-    void (*func)();
 } cmdToState_t;
 
 cmdToState_t default_commands[] = {
-    {"_i", IDLE, NULL},
-    {"_f", POWER_OFF, NULL},
-    {"_o", POWER_ON, NULL},
+    {"_i", IDLE},
+    {"_f", POWER_OFF},
+    {"_o", POWER_ON},
 };
 
 uint8_t num_default_commands = sizeof(default_commands)/sizeof(default_commands[0]);
@@ -275,6 +304,8 @@ void referenceHandler(uint8_t power_leg, uint8_t setting_position){
             for (uint8_t i = 0; i < num_tracking_vars; i++) {
                 if (strcmp(variable, tracking_vars[i].name) == 0) {
                     power_leg_settings[power_leg].tracking_variable = tracking_vars[i].address;
+                    power_leg_settings[power_leg].tracking_var_name = tracking_vars[i].name;
+                    power_leg_settings[power_leg].reference_value = reference_value;
                     break;
                 }
             }
@@ -327,9 +358,12 @@ void boolSettingsHandler(uint8_t power_leg, uint8_t setting_position)
 {
     if (strncmp(bufferstr + 7, "_on", 3) == 0) {
         power_leg_settings[power_leg].settings[setting_position] = BOOL_SETTING_ON;
-    }
-    else if (strncmp(bufferstr + 7, "_off", 4) == 0) {
+        if (setting_position == BOOL_CAPA)  spin.gpio.resetPin(power_leg_settings[power_leg].switches[CAPA_SWITCH_INDEX]);  //turns the capacitor switch ON
+        if (setting_position == BOOL_DRIVER)  spin.gpio.resetPin(power_leg_settings[power_leg].switches[DRIVER_SWITCH_INDEX]);  //turns the capacitor switch ON
+    } else if (strncmp(bufferstr + 7, "_off", 4) == 0) {
         power_leg_settings[power_leg].settings[setting_position] = BOOL_SETTING_OFF;
+        if (setting_position == BOOL_CAPA)  spin.gpio.setPin(power_leg_settings[power_leg].switches[CAPA_SWITCH_INDEX]);  //turns the capacitor switch ON
+        if (setting_position == BOOL_DRIVER)  spin.gpio.setPin(power_leg_settings[power_leg].switches[DRIVER_SWITCH_INDEX]);  //turns the capacitor switch ON
     }
     else {
         // Unknown command
@@ -353,30 +387,13 @@ void defaultHandler()
 }
 
 
-// // Function to parse the power communications
-// void powerHandler() {
-//     for(uint8_t i = 0; i < num_power_settings; i++) //iterates the default commands
-//     {
-//         if (strncmp(bufferstr, power_settings[i].cmd, strlen(power_settings[i].cmd)) == 0)
-//         {
-//             if (power_settings[i].func != NULL)
-//             {
-//                 power_settings[i].func(); //pointer to a function to do additional stuff if needed
-//             }
-//             return;
-//         }
-//     }
-//     printk("unknown power command %s\n", bufferstr);
-// }
-
-
 // Function to parse the power leg settings commands
 void powerLegSettingsHandler() {
 
     // Determine the power leg based on the received message
-    uint8_t power_leg = LEG_1; // Default to LEG_1
+    leg_t power_leg = LEG1; // Default to LEG1
     if (strncmp(bufferstr, "_LEG2_", strlen("_LEG2_")) == 0) {
-        power_leg = LEG_2;
+        power_leg = LEG2;
     } else if (strncmp(bufferstr, "_LEG1_", strlen("_LEG1_")) != 0) {
         printk("Unknown leg identifier\n");
         return;
@@ -416,10 +433,14 @@ void setup_routine()
 {
     data.enableTwistDefaultChannels();
     spin.version.setBoardVersion(SPIN_v_0_9);
-    twist.initAllBuck(); // initialize in buck mode
+    twist.initLegBuck(LEG1);
+    twist.initLegBuck(LEG2);
 
     spin.gpio.configurePin(LEG1_CAPA_DGND, OUTPUT);
     spin.gpio.configurePin(LEG2_CAPA_DGND, OUTPUT);
+    spin.gpio.configurePin(LEG1_DRIVER_SWITCH, OUTPUT);
+    spin.gpio.configurePin(LEG2_DRIVER_SWITCH, OUTPUT);
+
 
     float32_t GV1 = 0.044301359147286994;
     float32_t OV1 = -89.8291125470221;
@@ -450,6 +471,9 @@ void setup_routine()
     CommTask_num = task.createBackground(loop_communication_task);
     task.createCritical(&loop_control_task, control_task_period);
 
+    opalib_control_init_leg1_pid(kp, ki, kd, control_task_period);
+    opalib_control_init_leg2_pid(kp, ki, kd, control_task_period);
+
     task.startBackground(AppTask_num);
     task.startBackground(CommTask_num);
     task.startCritical();
@@ -468,12 +492,6 @@ void loop_communication_task()
         defaultHandler();
         // spin.led.turnOn();
         break;
-    // case 'p':
-    //     console_read_line();
-    //     printk("buffer str = %s\n", bufferstr);
-    //     powerHandler();
-    //     // counter = 0;
-    //     break;
     case 's':
         console_read_line();
         printk("buffer str = %s\n", bufferstr);
@@ -485,27 +503,6 @@ void loop_communication_task()
         printk("buffer str = %s\n", bufferstr);
         calibrationHandler();
         break;
-    // case 's':
-    //     printk("step mode 2\n");
-    //     mode = STEPMODE_2;
-    //     counter = 0;
-    //     spin.gpio.setPin(LEG1_CAPA_DGND);
-    //     spin.gpio.resetPin(LEG2_CAPA_DGND);
-    //     break;
-    // case 'w':
-    //     spin.gpio.setPin(LEG1_CAPA_DGND);
-    //     spin.gpio.setPin(LEG2_CAPA_DGND);
-    //     break;
-    // case 'x':
-    //     spin.gpio.resetPin(LEG1_CAPA_DGND);
-    //     spin.gpio.resetPin(LEG2_CAPA_DGND);
-    //     break;
-    // case 'u':
-    //     duty_cycle += 0.01;
-    //     break;
-    // case 'd':
-    //     duty_cycle -= 0.01;
-    //     break;
     default:
         break;
     }
@@ -523,35 +520,42 @@ void loop_application_task()
             }
             break;
         case POWER_OFF:
-            spin.led.turnOn();
+            spin.led.toggle();
             if(!print_done) {
                 printk("POWER OFF \n");
                 print_done = true;
             }
+            printk("[%d,%d,%d,%d,%d]:", power_leg_settings[LEG1].settings[0], power_leg_settings[LEG1].settings[1], power_leg_settings[LEG1].settings[2], power_leg_settings[LEG1].settings[3], power_leg_settings[LEG1].settings[4]);
+            printk("%f:", power_leg_settings[LEG1].duty_cycle);
+            printk("%f:", power_leg_settings[LEG1].reference_value);
+            printk("%s:", power_leg_settings[LEG1].tracking_var_name);
+            printk("%f:", tracking_vars[LEG1].address[0]);
+            printk("[%d,%d,%d,%d,%d]:", power_leg_settings[LEG2].settings[0], power_leg_settings[LEG2].settings[1], power_leg_settings[LEG2].settings[2], power_leg_settings[LEG2].settings[3], power_leg_settings[LEG2].settings[4]);
+            printk("%f:", power_leg_settings[LEG2].duty_cycle);
+            printk("%f:", power_leg_settings[LEG2].reference_value);
+            printk("%s:", power_leg_settings[LEG2].tracking_var_name);
+            printk("%f:", tracking_vars[LEG2].address[0]);
+            printk("\n");
             break;
         case POWER_ON:
+            spin.led.turnOn();
             if(!print_done) {
                 printk("POWER ON \n");
                 print_done = true;
             }
+            printk("%f:", V1_max);
+            printk("%f:", V2_max);
+            printk("%f:", V1_low_value);
+            printk("%f:", I1_low_value);
+            printk("%f:", I2_low_value);
+            printk("%f",  V2_low_value);
+            printk("%f:", V_high_value);
+            printk("%f:", I_high_value);
+            printk("\n");
+
             break;
         default:
             break;
-        // else if (mode == STEPMODE_1 || mode == STEPMODE_2)
-        // {
-            // spin.led.toggle();
-            // if(counter >= RECORD_SIZE){
-            //     printk("%f:", record_array[print_counter].I1_low_value);
-            //     printk("%f:", record_array[print_counter].I2_low_value);
-            //     printk("%f:", record_array[print_counter].Ihigh_value);
-            //     printk("%f:", record_array[print_counter].V1_low_value);
-            //     printk("%f:", record_array[print_counter].V2_low_value);
-            //     printk("%f:", record_array[print_counter].Vhigh_value);
-            //     printk("%d:", print_counter);
-            //     print_counter++;
-            //     if(print_counter > 30) print_counter = 0;
-            // }
-        // }
     }
 
     // acquisition_moment = acquisition_moment + 0.01;
@@ -559,17 +563,7 @@ void loop_application_task()
     // spin.setHrtimAdcTrigInterleaved(acquisition_moment);
     // printk("%f:", acquisition_moment);
 
-    // printk("%f:", duty_cycle);
-    // printk("%f:", V1_max);
-    // printk("%f:", V2_max);
-    // printk("%f:", I_high);
-    // printk("%f:", I1_low_value);
-    // printk("%f:", I2_low_value);
-    // printk("%f:", V_high);
-    // printk("%f:", V1_low_value);
-    // printk("%f\n", V2_low_value);
-
-    task.suspendBackgroundMs(100);
+     task.suspendBackgroundMs(100);
 }
 
 void loop_control_task()
@@ -601,57 +595,48 @@ void loop_control_task()
 
     switch(mode){
         case IDLE:
-            pwm_enable = false;
+        case POWER_OFF:
             twist.stopLeg(LEG1);
             twist.stopLeg(LEG2);
+            pwm_enable_leg_1 = false;
+            pwm_enable_leg_2 = false;
+            counter = 0;
+            V1_max  = 0;
+            V2_max  = 0;
             break;
-        // case BUCK:
-        // case BOOST:
-        //     // if (!pwm_enable)
-        //     // {
-        //     //     pwm_enable = true;
-        //     //     if(mode == STEPMODE_1) twist.startLeg(LEG1);
-        //     //     if(mode == STEPMODE_2) twist.startLeg(LEG2);
-        //     //     // spin.setInterleavedOn();
-        //     //     counter = 0;
-        //     //     V1_max  = 0;
-        //     //     V2_max  = 0;
-        //     // }
-        //     // if(mode == STEPMODE_1) twist.setLegDutyCycle(LEG1,duty_cycle);
-        //     // if(mode == STEPMODE_2) twist.setLegDutyCycle(LEG2,duty_cycle);
-        //     break;
-        // case CAPACITOR:
 
-        //     if(mode == STEPMODE_1) twist.setLegDutyCycle(LEG1,duty_cycle);
-        //     if(mode == STEPMODE_2) twist.setLegDutyCycle(LEG2,duty_cycle);
-        //     if (!pwm_enable)
-        //     {
-        //         pwm_enable = true;
-        //         if(mode == STEPMODE_1) twist.startLeg(LEG1);
-        //         if(mode == STEPMODE_2) twist.startLeg(LEG2);
-        //         // spin.setInterleavedOn();
-        //         counter = 0;
-        //         V1_max  = 0;
-        //         V2_max  = 0;
-        //     }
+        case POWER_ON:
 
-        //     if(mode == STEPMODE_1){
-        //         if(counter<RECORD_SIZE/2) spin.gpio.resetPin(LEG1_CAPA_DGND);
-        //         record_array[counter].I1_low_value = I1_low_value;
-        //         record_array[counter].V1_low_value = V1_low_value;
-        //     }
-        //     if(mode == STEPMODE_2) {
-        //         record_array[counter].V2_low_value = V2_low_value;
-        //         record_array[counter].I2_low_value = I2_low_value;
-        //     }
+            if(!pwm_enable_leg_1 && power_leg_settings[LEG1].settings[BOOL_LEG]) {twist.startLeg(LEG1); pwm_enable_leg_1 = true;}
+            if(!pwm_enable_leg_2 && power_leg_settings[LEG2].settings[BOOL_LEG]) {twist.startLeg(LEG2); pwm_enable_leg_2 = true;}
 
-        //     record_array[counter].Ihigh_value = I_high;
-        //     record_array[counter].Vhigh_value = V_high;
+            //calls the pid calculation if the converter in either in mode buck or boost
+            if(power_leg_settings[LEG1].settings[BOOL_BUCK] || power_leg_settings[LEG1].settings[BOOL_BOOST])
+                power_leg_settings[LEG1].duty_cycle = opalib_control_leg1_pid_calculation(power_leg_settings[LEG1].reference_value , tracking_vars[LEG1].address[0]);
 
-        //     if(V1_low_value>V1_max) V1_max = V1_low_value;
-        //     if(V2_low_value>V2_max) V2_max = V2_low_value;
+            if(power_leg_settings[LEG2].settings[BOOL_BUCK] || power_leg_settings[LEG2].settings[BOOL_BOOST])
+                power_leg_settings[LEG2].duty_cycle = opalib_control_leg1_pid_calculation(power_leg_settings[LEG2].reference_value , tracking_vars[LEG2].address[0]);
 
-        //     if(counter < RECORD_SIZE) counter++;
+            if(power_leg_settings[LEG1].settings[BOOL_LEG]){
+                if(power_leg_settings[LEG1].settings[BOOL_BOOST]){
+                    twist.setLegDutyCycle(LEG1, (1-power_leg_settings[LEG1].duty_cycle) ); //inverses the convention of the leg in case of changing from buck to boost
+                } else {
+                    twist.setLegDutyCycle(LEG1, power_leg_settings[LEG1].duty_cycle ); //uses the normal convention by default
+                }
+            }
+
+            if(power_leg_settings[LEG2].settings[BOOL_LEG]){
+                if(power_leg_settings[LEG2].settings[BOOL_BOOST]){
+                    twist.setLegDutyCycle(LEG2, (1-power_leg_settings[LEG2].duty_cycle) ); //inverses the convention of the leg in case of changing from buck to boost
+                }else{
+                    twist.setLegDutyCycle(LEG2, power_leg_settings[LEG2].duty_cycle); //uses the normal convention by default
+                }
+            }
+
+            if(V1_low_value>V1_max) V1_max = V1_low_value;  //gets the maximum V1 voltage value. This is used for the capacitor test
+            if(V2_low_value>V2_max) V2_max = V2_low_value;  //gets the maximum V2 voltage value. This is used for the capacitor test
+
+            break;
         default:
             break;
     }
